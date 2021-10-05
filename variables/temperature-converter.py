337930f38fc0fa3a083fa1af0136913aeaee0798
Daniel Burgos <dalbr1@hotmail.com>
2021-10-04 19:43:32 -0500

    Se añade programa para convetir la temperatura en grados centígrados a temperatura en grados Fahrenheit

---
+++ variables/temperature-converter.py
@@ -0,0 +1,3 @@
+temperature_celsius = input('Temperature on celsius degrees: ' )
+temperature_fahrenheit = (float(temperature_celsius) * 9 / 5) + 32
+print(temperature_celsius, 'ªC is ', temperature_fahrenheit, ' ªF')
